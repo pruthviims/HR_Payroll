8505e40024574cd48eb963d5ff3441dd7abe289c
pruthviims <pruthvigwda.77@gmail.com>
2026-04-03 20:36:03 +0530

    V1.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/payroll/',
+  base: './',
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
